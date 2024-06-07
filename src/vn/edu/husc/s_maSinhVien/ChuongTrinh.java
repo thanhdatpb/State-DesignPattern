7aed3e4e27db118068030e0b56eb002d11d25d8f
Thanh Dat <tthanhdat.pb@gmail.com>
2024-06-07 20:52:56 +0700

    update comment

--- src/vn/edu/husc/s_maSinhVien/ChuongTrinh.java
+++ src/vn/edu/husc/s_maSinhVien/ChuongTrinh.java
@@ -5,25 +5,25 @@ public class ChuongTrinh {
         // khởi tạo đối tượng điện thoại với trạng thái ban đầu là Chuẩn
         DienThoai dt = new DienThoai(new NormalState());
 
-        //thiết lập trạng thái Ngoài trời cho dt
+        //Thiết lập trạng thái Ngoài trời cho dt
         dt.setState(new OutdoorState());
 
-        //thử minh hoạ khi có tin nhắn đến cho dt
+        //Thử minh hoạ khi có tin nhắn đến cho dt
         dt.smsReceived();
 
-        //thử minh hoạ khi có sự kiện hệ thống cho dt
+        //Thử minh hoạ khi có sự kiện hệ thống cho dt
         dt.systemNotification();
 
-        //thử minh hoạ khi có cuộc gọi đến cho dt
+        //Thử minh hoạ khi có cuộc gọi đến cho dt
         dt.incomingCall();
 
-        //thiết lập trạng thái Yên lặng cho dt
+        //Thiết lập trạng thái Yên lặng cho dt
         dt.setState(new SilentState());
 
-        //thử minh hoạ khi có tin nhắn đến dt
+        //Thử minh hoạ khi có tin nhắn đến dt
         dt.smsReceived();
 
-        //thử minh hoạ khi có cuộc gọi đến cho dt
+        //Thử minh hoạ khi có cuộc gọi đến cho dt
         dt.incomingCall();
     }
 }
--- src/vn/edu/husc/s_maSinhVien/Utils.java
+++ src/vn/edu/husc/s_maSinhVien/Utils.java
@@ -2,12 +2,12 @@ package vn.edu.husc.s_maSinhVien;
 
 public class Utils {
     public static void rung() {
-        // thực hiện rung thiết bị với mức độ cho trước
+        //Thực hiện rung thiết bị với mức độ cho trước
         System.out.println("Rung điện thoại");
     }
 
     public static void phatAmThanh(int amLuong, int soLan) {
-        // thực hiện phát âm thanh BEEP BEEP với âm lượng và số lần cho trước
+        //Thực hiện phát âm thanh BEEP BEEP với âm lượng và số lần cho trước
         System.out.println("BEEP-BEEP với âm lượng " + amLuong + " trong " + soLan + " lần");
     }
 }
